1db1e34a44e8a51d8d8827e0594e58b95873e150
Christopher Bebry <invalidsyntax@gmail.com>
2013-05-15 13:26:46 -0400

    Added cake img and prepped Game a bit.

--- agentkiwi/agentkiwi/Game1.cs
+++ agentkiwi/agentkiwi/Game1.cs
@@ -11,14 +11,14 @@ using Microsoft.Xna.Framework.Media;
 
 namespace agentkiwi
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         CParticleMachine machine = null;
+
+        int screenWidth = 1280;
+        int screenHeight = 720;
 
         public Game1()
         {
@@ -26,68 +26,46 @@ namespace agentkiwi
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            graphics.PreferredBackBufferWidth = screenWidth;
+            graphics.PreferredBackBufferHeight = screenHeight;
+            graphics.IsFullScreen = false;
+            graphics.ApplyChanges();
+
+            Window.Title = "HAPPY BIRTHDAY ASHLEY FAIRWEATHER!";
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             CParticle.box = Content.Load<Texture2D>("box");
             machine = new CParticleMachine();
 
-            // TODO: use this.Content to load your game content here
+            Rectangle cakerect = new Rectangle(0, 0, screenWidth, screenHeight);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
-        }
+        protected override void UnloadContent()  {  }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
                 this.Exit();
+            }
 
             machine.update(gameTime);
-            // TODO: Add your update logic here
+
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
             spriteBatch.Begin();
             machine.draw(spriteBatch);
             spriteBatch.End();
