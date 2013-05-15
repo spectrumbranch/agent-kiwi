d2f8bf4798e4c5bdaea5552f0f53297ea4cef815
MGZero <steven.calandra@gmail.com>
2013-05-15 15:50:09 -0400

    NOW COMPLETE!!
    Signed-off-by: MGZero <steven.calandra@gmail.com>

---
+++ agentkiwi/agentkiwi/thefunguy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace agentkiwi
+{
+    class thefunguy
+    {
+        public static Texture2D fun;
+        public static Vector2 position = new Vector2(-740, 40);
+
+
+        public static void update(GameTime gameTime)
+        {
+            position.X += 6;
+        }
+
+        public static void draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(fun, new Rectangle((int)position.X, (int)position.Y, fun.Width, fun.Height), Color.White);
+        }
+
+    }
+}
